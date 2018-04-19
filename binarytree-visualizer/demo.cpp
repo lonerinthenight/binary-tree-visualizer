64bb9102783d680f94eb99003e92a4b5b264626f
qq765255166 <qq765255166@gmail.com>
2018-04-19 13:03:41 +0800

    upload

--- binarytree-visualizer/demo.cpp
+++ binarytree-visualizer/demo.cpp
@@ -2,11 +2,16 @@
 
 int main()
 {
-	// build a binary search tree.
+	// build a binary tree like this:
+	//        5
+	//       / \
+	//      2   7
+	//     / \   \
+	//    1   4   8
 	TreeNode *root = makeTree({ 5,2,7,1,4,null,8 });
 	// visualize the binary tree using GraphViz.
 	showTree(root);
-	// Delete the binary tree and the list.
+	// delete the binary tree.
 	delTree(root);
 	return 0;
 }
